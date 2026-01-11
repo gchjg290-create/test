5eb057f7ceb818094d97c5b823ce349319bdb1e9
gchjg290-create <gchjg290@gmail.com>
2026-01-11 20:36:20 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
-alert('for test XSS');
-document.body.innerHTML = 'XSS';
-document.body.appendChild(document.createTextNode('XSS'));
+//alert('for test XSS');
+//document.body.innerHTML = 'XSS';
+//document.body.appendChild(document.createTextNode('XSS'));
+window.__gadgetLoaded = true;
